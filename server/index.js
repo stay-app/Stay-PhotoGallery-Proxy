e061ef30e2344e8c0081af4441b1bbea592ac98c
TK <taehnkim@gmail.com>
2019-09-04 21:19:31 -0700

    Consolidated all services to proxy

--- server/index.js
+++ server/index.js
@@ -12,15 +12,24 @@ app.use(cors());
 app.disable('etag');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client')));
 
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../') + '/client/index.html');
-});
 
 app.get('/api/images/:listingid', (req, res) => {
   axios.get(`http://localhost:5000/api/images/${req.params.listingid}`)
   .then((data) => {
     res.status(200).send(data.data)
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send(err);
+  });
+});
+
+app.get('/api/reservation/search/:id', (req, res) => {
+  axios.get(`http://localhost:5001/api/reservation/search/${req.params.id}`)
+  .then((data) => {
+    res.status(200).send({ listing: data.data.listing[0], booking: data.data.booking });
   })
   .catch((err) => {
     console.log(err);
